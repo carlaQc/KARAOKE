adbb32f9b7c10add58b60ac58eaee4e9bdfe1e79
carlaQc <quenallatacarla@gmail.com>
2019-04-22 00:25:05 +0100

    añadiendo Schema

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -15,7 +15,6 @@ class UsersTableSeeder extends Seeder
         DB::table('users')->insert([
             'name' => 'Admin Admin',
             'email' => 'admin@argon.com',
-            'email_verified_at' => now(),
             'password' => Hash::make('secret'),
             'created_at' => now(),
             'updated_at' => now()
