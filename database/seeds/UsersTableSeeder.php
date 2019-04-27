86a80327c88a51f1fb19b7efe57aa9cdc3aaaeaf
carlaQc <quenallatacarla@gmail.com>
2019-04-27 22:48:03 +0100

    vista cliente

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -22,6 +22,7 @@ class UsersTableSeeder extends Seeder
             'created_at' => now(),
             'updated_at' => now()
         ]);
+            
         DB::table('users')->insert([
             'name' => 'oscar',
             'email' => 'oscar@gmail.com',
