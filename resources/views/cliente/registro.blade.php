f323aa07435e3d6292c405c3e3572216584b54a4
oz555 <kactus2525@gmail.com>
2019-04-28 11:08:00 -0400

    creacion de la vista de registro de cliente

---
+++ resources/views/cliente/registro.blade.php
@@ -0,0 +1,287 @@
+@extends('layouts.app')
+
+@section('content')
+    @include('layouts.headers.cards')
+    
+    <div class="container-fluid mt--7">
+        <div class="row">
+            <div class="col-xl-8 mb-5 mb-xl-0">
+                <div class="card bg-gradient-default shadow">
+                    <div class="card-header bg-transparent">
+                        
+        <form method="post" action="{{ route('cliente.update') }}" autocomplete="off">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                    {{ csrf_field() }}
+                    {{ method_field('PUT') }}
+                    <h6 class="heading-small text-muted mb-4">{{ __('Registrar cliente') }}</h6>
+                    <div class="pl-lg-4">
+                        <div class="form-group{{ $errors->has('name') ? ' has-danger' : '' }}">
+                            <label class="form-control-label" for="input-name">{{ __('Nombre') }}</label>
+                            <input type="text" name="name" id="input-name" class="form-control form-control-alternative{{ $errors->has('Nombre') ? ' is-invalid' : '' }}" placeholder="{{ __('Nombre') }}" value="" required autofocus>
+
+                            @if ($errors->has('name'))
+                                <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $errors->first('name') }}</strong>
+                                        </span>
+                            @endif
+                        </div>
+                        <div class="form-group{{ $errors->has('email') ? ' has-danger' : '' }}">
+                            <label class="form-control-label" for="input-email">{{ __('Correo') }}</label>
+                            <input type="email" name="email" id="input-email" class="form-control form-control-alternative{{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="{{ __('Email') }}" value="" required>
+
+                            @if ($errors->has('email'))
+                                <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $errors->first('email') }}</strong>
+                                        </span>
+                            @endif
+                        </div>
+
+
+                    </div>
+
+            </div>
+
+
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">CERRAR</button>
+                <button type="submit" class="btn btn-primary">{{ __('GUARDAR') }}</button>
+            </div>
+            </form>
+
+                </div>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col-xl-8 mb-5 mb-xl-0">
+                <div class="card shadow">
+                    <div class="card-header border-0">
+                        <div class="row align-items-center">
+                            <div class="col">
+                                <h3 class="mb-0">Page visits</h3>
+                            </div>
+                            <div class="col text-right">
+                                <a href="#!" class="btn btn-sm btn-primary">See all</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="table-responsive">
+                        <!-- Projects table -->
+                        <table class="table align-items-center table-flush">
+                            <thead class="thead-light">
+                                <tr>
+                                    <th scope="col">Page name</th>
+                                    <th scope="col">Visitors</th>
+                                    <th scope="col">Unique users</th>
+                                    <th scope="col">Bounce rate</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">
+                                        /argon/
+                                    </th>
+                                    <td>
+                                        4,569
+                                    </td>
+                                    <td>
+                                        340
+                                    </td>
+                                    <td>
+                                        <i class="fas fa-arrow-up text-success mr-3"></i> 46,53%
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">
+                                        /argon/index.html
+                                    </th>
+                                    <td>
+                                        3,985
+                                    </td>
+                                    <td>
+                                        319
+                                    </td>
+                                    <td>
+                                        <i class="fas fa-arrow-down text-warning mr-3"></i> 46,53%
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">
+                                        /argon/charts.html
+                                    </th>
+                                    <td>
+                                        3,513
+                                    </td>
+                                    <td>
+                                        294
+                                    </td>
+                                    <td>
+                                        <i class="fas fa-arrow-down text-warning mr-3"></i> 36,49%
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">
+                                        /argon/tables.html
+                                    </th>
+                                    <td>
+                                        2,050
+                                    </td>
+                                    <td>
+                                        147
+                                    </td>
+                                    <td>
+                                        <i class="fas fa-arrow-up text-success mr-3"></i> 50,87%
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">
+                                        /argon/profile.html
+                                    </th>
+                                    <td>
+                                        1,795
+                                    </td>
+                                    <td>
+                                        190
+                                    </td>
+                                    <td>
+                                        <i class="fas fa-arrow-down text-danger mr-3"></i> 46,53%
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4">
+                <div class="card shadow">
+                    <div class="card-header border-0">
+                        <div class="row align-items-center">
+                            <div class="col">
+                                <h3 class="mb-0">Social traffic</h3>
+                            </div>
+                            <div class="col text-right">
+                                <a href="#!" class="btn btn-sm btn-primary">See all</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="table-responsive">
+                        <!-- Projects table -->
+                        <table class="table align-items-center table-flush">
+                            <thead class="thead-light">
+                                <tr>
+                                    <th scope="col">Referral</th>
+                                    <th scope="col">Visitors</th>
+                                    <th scope="col"></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">
+                                        Facebook
+                                    </th>
+                                    <td>
+                                        1,480
+                                    </td>
+                                    <td>
+                                        <div class="d-flex align-items-center">
+                                            <span class="mr-2">60%</span>
+                                            <div>
+                                                <div class="progress">
+                                                <div class="progress-bar bg-gradient-danger" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;"></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">
+                                        Facebook
+                                    </th>
+                                    <td>
+                                        5,480
+                                    </td>
+                                    <td>
+                                        <div class="d-flex align-items-center">
+                                            <span class="mr-2">70%</span>
+                                            <div>
+                                                <div class="progress">
+                                                <div class="progress-bar bg-gradient-success" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width: 70%;"></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">
+                                        Google
+                                    </th>
+                                    <td>
+                                        4,807
+                                    </td>
+                                    <td>
+                                        <div class="d-flex align-items-center">
+                                            <span class="mr-2">80%</span>
+                                            <div>
+                                                <div class="progress">
+                                                <div class="progress-bar bg-gradient-primary" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%;"></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">
+                                        Instagram
+                                    </th>
+                                    <td>
+                                        3,678
+                                    </td>
+                                    <td>
+                                        <div class="d-flex align-items-center">
+                                            <span class="mr-2">75%</span>
+                                            <div>
+                                                <div class="progress">
+                                                    <div class="progress-bar bg-gradient-info" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%;"></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">
+                                        twitter
+                                    </th>
+                                    <td>
+                                        2,645
+                                    </td>
+                                    <td>
+                                        <div class="d-flex align-items-center">
+                                            <span class="mr-2">30%</span>
+                                            <div>
+                                                <div class="progress">
+                                                <div class="progress-bar bg-gradient-warning" role="progressbar" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100" style="width: 30%;"></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        @include('layouts.footers.auth')
+    </div>
+@endsection
+
+@push('js')
+    <script src="{{ asset('argon') }}/vendor/chart.js/dist/Chart.min.js"></script>
+    <script src="{{ asset('argon') }}/vendor/chart.js/dist/Chart.extension.js"></script>
+@endpush
