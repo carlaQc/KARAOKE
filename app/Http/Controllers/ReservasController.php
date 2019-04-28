aa5fae5a9448890444c3dfde14d26581fcc64d15
juankisss <juanhuanca917@gmail.com>
2019-04-27 23:36:12 -0400

    creacion del controlador reservas

---
+++ app/Http/Controllers/ReservasController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ReservasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    
+    public function index()
+    {
+        return view('Reservas.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    
+}
