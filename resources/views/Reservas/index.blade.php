de51597def920f38147ff7394231011a087fd217
juankisss <juanhuanca917@gmail.com>
2019-04-27 23:37:25 -0400

    creacion de las vistas reservas

---
+++ resources/views/Reservas/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app', ['title' => __('User Management')])
+
+@section('content')
+    @include('layouts.headers.cards')
+
+
+
+
+ 
+ @include('layouts.footers.auth')
+
+ @endsection
